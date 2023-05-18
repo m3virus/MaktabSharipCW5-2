0790233578dad31f94783ecb3dbeaf658f1635e2
mohammad_hasan <mryazdani98@outlook.com>
2023-05-18 16:33:04 +0330

    adding person

---
+++ ConsoleApp1/ConsoleApp1/person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class person
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int natinalCode { get; set; }
+        public int Mobile { get; set; }
+        public string address { get; set; }
+        public int age { get; set; }
+
+    }
+}
